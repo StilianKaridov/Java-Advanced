8c5e6253616c57eb36c6c6ce52f3e3b9850b639f
StilianKaridov <karidovstilian@gmail.com>
2022-06-06 13:20:06 +0300

    Functional Programming tasks

--- src/FunctionalProgramming/AppliedArithmetics.java
+++ src/FunctionalProgramming/AppliedArithmetics.java
@@ -4,31 +4,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 public class AppliedArithmetics {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toList();
-        Consumer<List<Integer>> printer = n -> n.listIterator().forEachRemaining(e -> System.out.print(e + " "));
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        Consumer<Integer> printer = n -> System.out.printf("%d ", n);
+        UnaryOperator<List<Integer>> add = numbersList -> numbersList.stream().map(e -> e + 1).collect(Collectors.toList());
+        UnaryOperator<List<Integer>> subtract = numbersList -> numbersList.stream().map(e -> e - 1).collect(Collectors.toList());
+        UnaryOperator<List<Integer>> multiply = numbersList -> numbersList.stream().map(e -> e * 2).collect(Collectors.toList());
+
         String command = scanner.nextLine();
         while (!command.equals("end")) {
             switch (command) {
                 case "add":
-
+                    numbers = add.apply(numbers);
                     break;
                 case "multiply":
+                    numbers = multiply.apply(numbers);
                     break;
                 case "subtract":
+                    numbers = subtract.apply(numbers);
                     break;
                 case "print":
-                    printer.accept(numbers);
+                    numbers.forEach(printer);
                     System.out.println();
                     break;
             }
             command = scanner.nextLine();
         }
-        printer.accept(numbers);
     }
 }
