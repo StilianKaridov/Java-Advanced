9cd9607839661208ac5e0d85a379216f9f025b0d
StilianKaridov <karidovstilian@gmail.com>
2022-06-06 11:12:00 +0300

    Changed toList method with collect(Collectors.toList)

--- src/FunctionalProgramming/ReverseAndExclude.java
+++ src/FunctionalProgramming/ReverseAndExclude.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class ReverseAndExclude {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> list = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toList();
+        List<Integer> list = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
         int n = Integer.parseInt(scanner.nextLine());
         List<Integer> divisible = list.stream().filter(e -> e % n != 0).collect(Collectors.toList());
         Collections.reverse(divisible);
