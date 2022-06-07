e6728d974947281fb96bc5b00919abc2d3dbfa82
StilianKaridov <karidovstilian@gmail.com>
2022-06-07 14:46:48 +0300

    Tasks from Labs

---
+++ src/StacksAndQueues_Lab/BrowserHistory.java
@@ -0,0 +1,32 @@
+package StacksAndQueues_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> urls = new ArrayDeque<>();
+        String command = scanner.nextLine();
+        String currentURL = "blank";
+        while (!command.equals("Home")) {
+
+            if (command.equals("back")) {
+                if (!urls.isEmpty()) {
+                    currentURL = urls.pop();
+                } else {
+                    System.out.println("no previous URLs");
+                    command = scanner.nextLine();
+                    continue;
+                }
+            } else {
+                if (!currentURL.equals("blank")) {
+                    urls.push(currentURL);
+                }
+                currentURL = command;
+            }
+            System.out.println(currentURL);
+            command = scanner.nextLine();
+        }
+    }
+}
