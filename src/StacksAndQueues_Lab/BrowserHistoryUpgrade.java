a75f44421f8a8134f577715fbf1531e81e0afe8a
StilianKaridov <karidovstilian@gmail.com>
2022-06-07 15:43:09 +0300

    Last two tasks from Lab

---
+++ src/StacksAndQueues_Lab/BrowserHistoryUpgrade.java
@@ -0,0 +1,37 @@
+package StacksAndQueues_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> urls = new ArrayDeque<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        String command = scanner.nextLine();
+        while (!command.equals("Home")) {
+            if (command.equals("back")) {
+                if (urls.size() > 1) {
+                    String currentURL = urls.pop();
+                    queue.push(currentURL);
+                    System.out.println(urls.peek());
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else if (command.equals("forward")) {
+                if (!queue.isEmpty()) {
+                    String currentForward = queue.poll();
+                    System.out.println(currentForward);
+                    urls.push(currentForward);
+                } else {
+                    System.out.println("no next URLs");
+                }
+            } else {
+                urls.push(command);
+                System.out.println(command);
+                queue.clear();
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
