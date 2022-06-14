218fa334484c1ceed1723eff722cba8686c88107
StilianKaridov <karidovstilian@gmail.com>
2022-06-14 18:53:38 +0300

    Generics exercise

---
+++ src/Generics/GenericBox/Main.java
@@ -0,0 +1,17 @@
+package Generics.GenericBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box;
+
+        while (n-- > 0) {
+            String actualString = scanner.nextLine();
+            box = new Box<>(actualString);
+            System.out.println(box.toString());
+        }
+    }
+}
---
+++ src/Generics/GenericCountMethod/Box.java
@@ -0,0 +1,16 @@
+package Generics.GenericCountMethod;
+
+import java.util.List;
+
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public static <T extends Comparable<T>> long genericCount(List<T> list, T type){
+        return list.stream().filter(e->e.compareTo(type) > 0).count();
+    }
+
+}
