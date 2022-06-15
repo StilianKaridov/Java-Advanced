5d47cfee0f14d785bb618acc09adac6c1ad2dddb
StilianKaridov <karidovstilian@gmail.com>
2022-06-15 16:14:31 +0300

    Iterators and Comparators Lab

---
+++ src/IteratorsAndComparators_Lab/BookComparator/BookComparator.java
@@ -0,0 +1,12 @@
+package IteratorsAndComparators_Lab.BookComparator;
+
+import IteratorsAndComparators_Lab.Book.Book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.compareTo(o2);
+    }
+}
