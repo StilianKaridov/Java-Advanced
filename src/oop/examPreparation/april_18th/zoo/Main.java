98a7b68abd00ae57b076594af4050839a744f023
StilianKaridov <karidovstilian@gmail.com>
2022-08-08 16:29:40 +0300

    Exam preparation

--- src/oop/examPreparation/april_18th/zoo/Main.java
+++ src/oop/examPreparation/april_18th/zoo/Main.java
@@ -4,7 +4,6 @@ import oop.examPreparation.april_18th.zoo.core.Engine;
 import oop.examPreparation.april_18th.zoo.core.EngineImpl;
 
 public class Main {
-
     public static void main(String[] args) {
         Engine engine = new EngineImpl();
         engine.run();
